481027a5710304e0abcf4ecf81de54324e5f1216
caijinhao <531003539@qq.com>
2022-03-15 11:32:29 +0800

    modify reference

--- modules/libs/Jh.AbpExtensions/Jh.Abp.EntityFrameworkCore/JhAbpEntityFrameworkCoreModule.cs
+++ modules/libs/Jh.AbpExtensions/Jh.Abp.EntityFrameworkCore/JhAbpEntityFrameworkCoreModule.cs
@@ -1,10 +1,4 @@
-﻿using Jh.Abp.Application.Contracts;
-using Jh.Abp.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Jh.Abp.Domain;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.Modularity;
 
@@ -12,7 +6,6 @@ namespace Jh.Abp.EntityFrameworkCore
 {
     [DependsOn(
         typeof(JhAbpDomainModule),
-        typeof(JhAbpContractsModule),
         typeof(AbpEntityFrameworkCoreModule)
        )]
     public class JhAbpEntityFrameworkCoreModule : Volo.Abp.Modularity.AbpModule
