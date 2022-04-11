9ff548a8591494c281888d7ce78fac3c178fb81a
caijinhao <531003539@qq.com>
2022-04-11 15:17:21 +0800

    add account login service

--- modules/admin-react/jhabpadmin/config/proxy.ts
+++ modules/admin-react/jhabpadmin/config/proxy.ts
@@ -8,13 +8,13 @@
  */
 export default {
   dev: {
-    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-    '/api/': {
+    '/identity': {
       // 要代理的地址
-      target: 'https://preview.pro.ant.design',
+      target: 'https://localhost:6201',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
+      pathRewrite: { '^/identity': '' },
     },
   },
   test: {
