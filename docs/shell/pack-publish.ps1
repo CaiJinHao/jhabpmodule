200d3f6f7e943dd221c4e4a90a69c8708b3d9a91
Home-hostpc <531003539@qq.com>
2022-04-11 10:24:07 +0800

    modify pack publish path

--- docs/shell/pack-publish.ps1
+++ docs/shell/pack-publish.ps1
@@ -5,7 +5,7 @@ using namespace System.Xml;
 param (
     [string] $apiKey,
     [string] $execPath = '../../',
-    [string] $outPath = 'G:\Publish\nuget-local',
+    [string] $outPath = 'E:\Publish\nuget-local',
     [string] $publishSource = "https://api.nuget.org/v3/index.json"
 )
 
