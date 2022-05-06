27f3e3c45747c1191c609484d8e39c1c087d3d4d
Home-hostpc <531003539@qq.com>
2022-05-06 17:35:19 +0800

    login

---
+++ modules/libs/QuickComponents/Jh.Abp.QuickComponents/Jh/Abp/Providers/AppBrandingProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace Wj.CarMsg.Extentions
+{
+    [Dependency(ReplaceServices = true)]
+    public class AppBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName { get; }
+        public override string LogoUrl { get; }
+        public AppBrandingProvider(IConfiguration configuration)
+        {
+            AppName = configuration["App:AppName"];
+            LogoUrl = configuration["App:LogoUrl"];
+        }
+    }
+}
