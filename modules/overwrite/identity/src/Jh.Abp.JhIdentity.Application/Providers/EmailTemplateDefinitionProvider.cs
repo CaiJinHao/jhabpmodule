1134364c20c45a87513648c50bcbcd0447772762
caijinhao <531003539@qq.com>
2022-07-08 17:13:30 +0800

    模板测试

---
+++ modules/overwrite/identity/src/Jh.Abp.JhIdentity.Application/Providers/EmailTemplateDefinitionProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.TextTemplating;
+
+namespace Jh.Abp.JhIdentity.Providers
+{
+    public class EmailTemplateDefinitionProvider : TemplateDefinitionProvider
+    {
+        //public override void Define(ITemplateDefinitionContext context)
+        //{
+        //    context.Add(
+        //    new TemplateDefinition(
+        //        AccountEmailTemplates.PasswordResetLink,
+        //        displayName: LocalizableString.Create<AccountResource>($"TextTemplate:{AccountEmailTemplates.PasswordResetLink}"),
+        //        layout: StandardEmailTemplates.Layout,
+        //        localizationResource: typeof(AccountResource)
+        //    ).WithVirtualFilePath("/Volo/Abp/Account/Emailing/Templates/PasswordResetLink.tpl", true)
+        //    );
+        //}
+        public override void Define(ITemplateDefinitionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
