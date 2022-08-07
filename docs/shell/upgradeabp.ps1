25dcea4d337a07a7973f0ec4bed0dd8a4f99aa3c
home-pc <531003539@qq.com>
2022-08-07 13:34:40 +0800

    upgrade 5.3.3

---
+++ docs/shell/upgradeabp.ps1
@@ -0,0 +1,5 @@
+# 升级abp框架
+abp update --solution-path 'G:\mycode\jhmodule\modules\libs' --nuget
+abp update --solution-path 'G:\mycode\jhmodule\modules\module_extend\workflow' --nuget
+abp update --solution-path 'G:\mycode\jhmodule\modules\overwrite\identity' --nuget
+
