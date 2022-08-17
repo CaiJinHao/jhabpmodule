9ac6c538244ed347274b9a37c2444c9a081080d4
caijinhao <531003539@qq.com>
2022-08-17 14:36:21 +0800

    role not ok

--- modules/overwrite/identity/src/Jh.Abp.JhIdentity.MongoDB/Identity/IdentityUser/EfCoreIdentityUserRepositoryOverride.cs
+++ modules/overwrite/identity/src/Jh.Abp.JhIdentity.MongoDB/Identity/IdentityUser/EfCoreIdentityUserRepositoryOverride.cs
@@ -21,7 +21,7 @@ namespace Jh.Abp.JhIdentity
         }
 
 
-       /* /// <summary>
+        /// <summary>
         /// 针对用户组织的处理
         /// </summary>
         /// <param name="id"></param>
@@ -29,38 +29,8 @@ namespace Jh.Abp.JhIdentity
         /// <returns></returns>
         public override async Task<List<string>> GetRoleNamesAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            var IdentityUserRoles = await GetMongoQueryableAsync<IdentityUserRole>();
-            var IdentityRoles = await GetMongoQueryableAsync<IdentityRole>();
-            var OrganizationUnitRoles = await GetMongoQueryableAsync<OrganizationUnitRole>();
-            var IdentityUserOrganizationUnits = await GetMongoQueryableAsync<IdentityUserOrganizationUnit>();
-            var OrganizationUnits = await GetMongoQueryableAsync<OrganizationUnit>();
-            //用户所属的角色
-            var query = from userRole in IdentityUserRoles
-                        join role in IdentityRoles on userRole.RoleId equals role.Id
-                        where userRole.UserId == id
-                        select role.Id;
-
-            IQueryable<Guid> resultRoleIds;
-            //这个用户所属的组织列表
-            var organizationUnitIds = IdentityUserOrganizationUnits.Where(q => q.UserId == id).Select(q => q.OrganizationUnitId).ToArray();
-            if (organizationUnitIds.Any())
-            {
-                //指定组织列表下的角色
-                var organizationRoleIds = await (
-                    from ouRole in OrganizationUnitRoles
-                    join ou in OrganizationUnits on ouRole.OrganizationUnitId equals ou.Id
-                    where organizationUnitIds.Contains(ouRole.OrganizationUnitId)
-                    select ouRole.RoleId
-                ).ToListAsync(GetCancellationToken(cancellationToken));
-
-                resultRoleIds = query.Where(a => organizationRoleIds.Contains(a));//用户所属角色 并且所属角色在所属组织下
-            }
-            else
-            {
-                resultRoleIds = query;
-            }
-            var resultQuery = IdentityRoles.Where(r => resultRoleIds.Contains(r.Id)).Select(n => n.Name);
-            return await resultQuery.ToListAsync(GetCancellationToken(cancellationToken));
+            var userRoleIds = await GetUserRoleIdsAsync(id, cancellationToken);
+            return await (await GetMongoQueryableAsync<IdentityRole>()).Where(a => userRoleIds.Contains(a.Id)).Select(a => a.Name).ToListAsync();
         }
 
         /// <summary>
@@ -72,42 +42,24 @@ namespace Jh.Abp.JhIdentity
         /// <returns></returns>
         public override async Task<List<IdentityRole>> GetRolesAsync(Guid id, bool includeDetails = false, CancellationToken cancellationToken = default)
         {
-            var IdentityUserRoles = await GetMongoQueryableAsync<IdentityUserRole>();
-            var IdentityRoles = await GetMongoQueryableAsync<IdentityRole>();
-            var IdentityUserOrganizationUnits = await GetMongoQueryableAsync<IdentityUserOrganizationUnit>();
-            var OrganizationUnits = await GetMongoQueryableAsync<OrganizationUnit>();
-            var OrganizationUnitRoles = await GetMongoQueryableAsync<OrganizationUnitRole>();
-            //用户所属的角色
-            var query = from userRole in IdentityUserRoles
-                        join role in IdentityRoles on userRole.RoleId equals role.Id
-                        where userRole.UserId == id
-                        select role;
+            var userRoleIds = await GetUserRoleIdsAsync(id,cancellationToken);
+            return await (await GetMongoQueryableAsync<IdentityRole>()).Where(a => userRoleIds.Contains(a.Id)).ToListAsync();
+        }
 
-            //TODO: Needs improvement
+        private async Task<Guid[]> GetUserRoleIdsAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var user = await GetAsync(id, cancellationToken: cancellationToken);
+            //用户所属的角色
+            var userRoleIds = user.Roles.Select(a => a.RoleId).ToArray();
             //这个用户所属的组织列表
-            var userOrganizationsQuery = from userOrg in IdentityUserOrganizationUnits
-                                         join ou in OrganizationUnits on userOrg.OrganizationUnitId equals ou.Id
-                                         where userOrg.UserId == id
-                                         select ou;
-            IQueryable<IdentityRole> resultQuery;
-            if (userOrganizationsQuery.Any())
+            var userOrganizationUnitIds = user.OrganizationUnits.Select(a => a.OrganizationUnitId);
+            if (userOrganizationUnitIds.Any())
             {
                 //指定组织列表下的角色
-                var orgUserRoleQuery = OrganizationUnitRoles
-                    .Where(q => userOrganizationsQuery
-                    .Select(t => t.Id)
-                    .Contains(q.OrganizationUnitId))
-                    .Select(t => t.RoleId)
-                    .ToArray();
-                //var orgRoles = dbContext.Roles.Where(q => orgUserRoleQuery.Contains(q.Id));
-                //var resultQuery = query.Union(orgRoles);
-                resultQuery = query.Where(a => orgUserRoleQuery.Contains(a.Id));//用户所属角色 并且所属角色在所属组织下
+                var organizationUnit = await (await GetMongoQueryableAsync<OrganizationUnit>()).Where(a => userOrganizationUnitIds.Contains(a.Id)).ToListAsync();
+                userRoleIds = organizationUnit.SelectMany(a => a.Roles).Select(a => a.RoleId).Where(a => userRoleIds.Contains(a)).ToArray();
             }
-            else
-            {
-                resultQuery = query;
-            }
-            return await resultQuery.ToListAsync(GetCancellationToken(cancellationToken));
-        }*/
+            return userRoleIds;
+        }
     }
 }
